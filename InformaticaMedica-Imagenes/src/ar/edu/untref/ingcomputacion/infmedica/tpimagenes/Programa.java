ea304f5260f802d23f074cc3f19bac2d877f25b5
Maribel Maisano <maribel.maisano@gmail.com>
2014-10-01 02:06:32 -0300

    Programa que lee una imagen, detecta los bordes y la guarda en disco

---
+++ InformaticaMedica-Imagenes/src/ar/edu/untref/ingcomputacion/infmedica/tpimagenes/Programa.java
@@ -0,0 +1,74 @@
+package ar.edu.untref.ingcomputacion.infmedica.tpimagenes;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.media.jai.JAI;
+import javax.media.jai.PlanarImage;
+
+public class Programa {
+
+	private static final String RUTA_IMAGEN = "resources/radiografia.jpg";
+	private static final String JAI_OPERADOR_CARGA_IMAGEN = "fileload";
+	private static final String OUTPUT_RUTA = "resources/output";
+	private static final String OUTPUT_EXTENSION_IMAGEN = "jpg";
+	
+	//Filtros
+	private static final String JAI_OPERADOR_DETECCION_DE_BORDES = "GradientMagnitude";
+
+	static {
+		inicializar();
+	}
+
+	private static void inicializar() {
+		System.setProperty("com.sun.media.jai.disableMediaLib", "true");
+	}
+	
+	public static void main(String[] args) {
+		
+		RenderedImage imagenOriginal = JAI.create(JAI_OPERADOR_CARGA_IMAGEN, RUTA_IMAGEN);
+		aplicarFiltro(imagenOriginal, JAI_OPERADOR_DETECCION_DE_BORDES);
+	}
+
+	private static void aplicarFiltro(RenderedImage imagenOriginal, String filtro) {
+		
+		System.out.println("Aplicando filtro...");
+		PlanarImage imagenInvertida = JAI.create(filtro, imagenOriginal);
+		BufferedImage bufferedImage = imagenInvertida.getAsBufferedImage();
+		
+		guardarImagen(bufferedImage);
+	}
+
+	private static void guardarImagen(BufferedImage bufferedImage) {
+		
+		System.out.println("Guardando imagen...");
+		
+		String nombreImagen = obtenerNombreArchivoOutput();
+		
+		try (FileOutputStream fileOutputStream = new FileOutputStream(nombreImagen);
+			 BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream))
+		{
+			ImageIO.write(bufferedImage, OUTPUT_EXTENSION_IMAGEN, fileOutputStream);
+			System.out.println("Imagen guardada exitosamente con nombre: " + nombreImagen);
+			
+		}catch (IOException e) {
+			System.err.println("Ocurrió un error guardando la imagen: " + e);
+		}
+	}
+
+	private static String obtenerNombreArchivoOutput() {
+		
+		StringBuilder nombreArchivoBuilder = new StringBuilder();
+		nombreArchivoBuilder.append(OUTPUT_RUTA);
+		nombreArchivoBuilder.append("_");
+		nombreArchivoBuilder.append(String.valueOf(System.currentTimeMillis()));
+		nombreArchivoBuilder.append(".");
+		nombreArchivoBuilder.append(OUTPUT_EXTENSION_IMAGEN);
+		
+		return nombreArchivoBuilder.toString();
+	}
+}
